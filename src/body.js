259062b8cd99e033324a564f3c2ebeb0236eeabf
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-03 19:14:11 +0200

    Home Page body added

--- src/body.js
+++ src/body.js
@@ -1,5 +1,36 @@
 // Import page for each different menu
 
-export default function createBody() {
+export default function createHomePage() {
+  const contentDiv = document.getElementById('content');
+  const homeDiv = document.createElement('div');
+  homeDiv.setAttribute('id', 'home');
+  contentDiv.appendChild(homeDiv);
 
+  topHomePage();
+  leftHomePage();
+  rightHomePage();
+}
+
+function topHomePage() {
+  const homeDiv = document.getElementById('home');
+  const homeTop = document.createElement('div');
+  homeTop.setAttribute('id', 'home-top');
+  homeTop.innerText = 'Welcome To The Three Broomsticks';
+  homeDiv.appendChild(homeTop);
+}
+
+function leftHomePage() {
+  const homeDiv = document.getElementById('home');
+  const homeLeft = document.createElement('div');
+  homeLeft.setAttribute('id', 'home-left');
+  homeLeft.innerText = 'Home of the Finest Butterbeer!';
+  homeDiv.appendChild(homeLeft);
+}
+
+function rightHomePage() {
+  const homeDiv = document.getElementById('home');
+  const homeRight = document.createElement('div');
+  homeRight.setAttribute('id', 'home-right');
+  homeRight.innerText = 'Student Specials';
+  homeDiv.appendChild(homeRight);
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import createHeader from "./header";
+import createHomePage from "./body";
 import createFooter from "./footer";
 
 function createPage() {
   createHeader();
-
+  createHomePage();
   createFooter();
 }
 
