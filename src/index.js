2c8b09b0d683b495c43827cfc857551d277feb38
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-04 11:53:32 +0200

    Imported about and contact functions to index,js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import createHeader from "./header";
 import createHomePage from "./home";
 import createMenu from "./menu";
+import createAbout from "./about";
+import createContact from "./contact";
 import createFooter from "./footer";
 import './style.css';
 
@@ -8,6 +10,8 @@ function createPage() {
   createHeader();
   createHomePage();
   createMenu();
+  createAbout();
+  createContact();
   createFooter();
 }
 
