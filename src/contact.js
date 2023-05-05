8f50b6b11eb1c255a5ff29a2d75e0a5f4e2d1c79
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-05 21:59:51 +0200

    Contact rough section added

--- src/contact.js
+++ src/contact.js
@@ -22,9 +22,39 @@ function contactBody() {
   const contactDiv = document.getElementById('contact');
   const contactBody = document.createElement('div');
   contactBody.setAttribute('id', 'contact-body');
-  contactBody.innerText = "Contact information...";
   contactDiv.appendChild(contactBody);
+
+  contactContent();
 }
+
+function contactContent() {
+  const contactBody = document.getElementById('contact-body');
+  const contactContent = document.createElement('div');
+  contactContent.setAttribute('id', 'contact-content');
+  contactBody.appendChild(contactContent);
+
+
+  const contactContentHeader = document.createElement('div');
+  contactContentHeader.setAttribute('id', 'contact-content-header');
+  contactContentHeader.innerText = "You Can Reach Us At The Following:";
+  contactContent.appendChild(contactContentHeader);
+
+  const contactContentP1 = document.createElement('div');
+  contactContentP1.setAttribute('id', 'contact-content-P1');
+  contactContentP1.innerText = "Contact number: +351 151 2352";
+  contactContent.appendChild(contactContentP1);  
+
+  const contactContentP2 = document.createElement('div');
+  contactContentP2.setAttribute('id', 'contact-content-P2');
+  contactContentP2.innerText = "Email: the3broomsticks@wizarding.com";
+  contactContent.appendChild(contactContentP2);  
+
+  const contactContentP3 = document.createElement('div');
+  contactContentP3.setAttribute('id', 'contact-content-P3');
+  contactContentP3.innerText = "Owl Address: 6 Aberwith Street, Hogsmeade, Scotland";
+  contactContent.appendChild(contactContentP3);
+}
+
 
 export function hideContact() {
   const contactDiv = document.getElementById('contact');
